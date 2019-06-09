29c46cbb093753d93b34e2a2cbf3368b4469da4d
Tomebuddy <tomebuddy@protonmail.com>
2019-06-09 15:14:00 +0200

    Complete the DescriptorContract with missing methods

--- src/Vivaldi/Description/DescriptorContract.php
+++ src/Vivaldi/Description/DescriptorContract.php
@@ -10,4 +10,18 @@ interface DescriptorContract
      * @return array
      */
     public function getItems();
+
+    /**
+     * Retrieve an item
+     *
+     * @return array
+     */
+    public function getItem($name);
+
+    /**
+     * Check that an item exists
+     *
+     * @return array
+     */
+    public function itemExists($name);
 }
